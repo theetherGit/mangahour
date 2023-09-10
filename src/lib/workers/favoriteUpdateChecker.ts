9ffce720f551757d6d75cbfc387152b2a04c505f
Shivam Meena <meenashivam9650@gmail.com>
2023-09-10 17:23:42 +0530

    better worker

--- src/lib/workers/favoriteUpdateChecker.ts
+++ src/lib/workers/favoriteUpdateChecker.ts
@@ -1,16 +1,20 @@
 import {db} from "$lib/db";
 
+const fetchUpdate = async (manga: any) => {
+    const response = await fetch(`/api/manga?id=${manga.id}&slug=${manga.slug}`);
+    if (response.ok) {
+        const manga = (await response.json()).manga;
+        db.favouriteManga.update(manga.id, {
+            lastUpdated: new Date(manga['last_chapter_created_at'])
+        })
+    }
+};
+
 onmessage = async (e) => {
     const favManga = await db.favouriteManga.toArray();
     if (favManga.length) {
         for (const manga of favManga) {
-            const response = await fetch(`/api/manga?id=${manga.id}&slug=${manga.slug}`);
-            if (response.ok) {
-                const manga = (await response.json()).manga;
-                db.favouriteManga.update(manga.id, {
-                    lastUpdated: new Date(manga['last_chapter_created_at'])
-                })
-            }
+            fetchUpdate(manga)
         }
     }
 };
