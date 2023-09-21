232c4e0ddbb7d7e16b54c51672b7e82d5d698a24
Shivam Meena <meenashivam9650@gmail.com>
2023-09-21 19:27:30 +0530

    Last updated fix

--- src/lib/workers/favoriteUpdateChecker.ts
+++ src/lib/workers/favoriteUpdateChecker.ts
@@ -4,7 +4,7 @@ const fetchUpdate = async (manga: any) => {
 	const response = await fetch(`/api/manga?id=${manga.id}&slug=${manga.slug}`);
 	if (response.ok) {
 		const manga = (await response.json()).manga;
-		db.favouriteManga.update(manga.id, {
+		db.favouriteManga.update(manga.id.toString(), {
 			lastUpdated: new Date(manga['last_chapter_created_at'])
 		});
 	}
