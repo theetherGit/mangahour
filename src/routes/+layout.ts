d23394e6b97cad6d12f3a4ca1962394328bb36df
Shivam Meena <meenashivam9650@gmail.com>
2024-03-13 11:37:38 +0530

    version bump and ssr only

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,8 @@
 import type { LayoutLoad } from './$types';
 import { redirect } from '@sveltejs/kit';
 
+export const ssr = true;
+
 export const load: LayoutLoad = async ({ url }) => {
 	if (url.pathname === '/') {
 		throw redirect(302, '/home');
